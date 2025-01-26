04a1a2d6db10757155e2bdc75e466869b87191e9
Roshami <grtmybusiness@gmail.com>
2025-01-26 18:54:53 +0530

    instaled tailwind css ❤

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import  ProductCard  from './components/productCard'
 
+
 function App() {
   
 
   return (
     <>
-      <ProductCard name="Audio Setup" price="3250/-" description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, autem." photoURL="https://static.independent.co.uk/2024/09/19/14/Bluetooth-speaker-hero-indybest.jpg?quality=75&width=640&auto=webp" />
+    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 
-      <ProductCard name="Commercial Audio" price="3000/-" description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, autem." photoURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvnZG3qrdzRGoE422EeQLs4qg04Epcnnj8bw&s" />
+      <div className='bg-red-500 w-12 h-12'></div>
+
+      
     </>
   )
 }
