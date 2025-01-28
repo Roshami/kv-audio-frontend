0c3cb58d6340609b60ee24ec9ba0421393883f56
Roshami <grtmybusiness@gmail.com>
2025-01-28 17:40:48 +0530

    created basic home page

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
+import Home from "./home";
+import Contact from "./contact";
+import Gallery from "./gallery";
+import Items from "./items";
+import ErrorNotFound from "./error";
 
 export default function HomePage() {
     return (
-        <div className="w-full h-full flex justify-center">
-            <Header/>
-        </div>
+        <>
+            <Header />
+            <div className="w-full h-[calc(100vh-100px)]">
+                <Routes path="/*">
+                    <Route path="/" element={<Home />}></Route>
+                    <Route  path="/contact" element={<Contact />}></Route>
+                    <Route path="/gallery" element={<Gallery />}></Route>
+                    <Route path="/items" element={<Items />}></Route>
+                    <Route path="/*" element={<ErrorNotFound/>}></Route>
+                </Routes>
+                
+            </div>
+        </>
     )
 }
