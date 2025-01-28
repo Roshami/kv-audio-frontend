a720be9d022e6539bf2c6c33d9692182583458b5
Roshami <grtmybusiness@gmail.com>
2025-01-28 14:58:32 +0530

    added basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import AdminPage from './components/adminPage'
-import ProductCard from './components/productCard'
-
 
 function App() {
 
 
   return (
     <>
-      <div>
-        <AdminPage />
-      </div>
+      <BrowserRouter>
+        <Routes path="/*">
+          <Route path="/admin/*" element={<AdminPage />} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
