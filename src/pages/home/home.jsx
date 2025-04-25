1958c626a8c07e9a9aa01ac6c9cd7075bf665556
Roshami <grtmybusiness@gmail.com>
2025-04-25 20:10:02 +0530

    created card slider

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,34 @@
+import { Link } from 'react-router-dom'
+import './home.css'
+import CardSlider from '../../components/CardSlider'
+
+
 export default function Home() {
     return (
-        <div>HomePage</div>
+        <div className="w-full h-full">
+
+            <div className="hero-bg-picture w-full h-[550px] md:h-[350px]  flex flex-col items-center justify-center">
+                <div className="w-full h-full bg-black/50 flex flex-col items-center justify-center px-[20px] pt-[20px]">
+
+                    <h1 className="text-5xl font-bold text-white text-center">Premium Audio Rentals for Every Event</h1>
+                    <p className="text-white text-center text-2xl">From studio mics to festival speakers,we’ve got you covered.</p>
+
+                    <div className='flex flex-col sm:flex-row sm:gap-10'>
+                        <Link to="/items" className="w-[200px] h-[50px] bg-blue-500 text-white px-5 py-2 rounded-lg hover:bg-blue-600 transition-transform transform hover:scale-105 mt-5 text-center text-2xl"> Order Now</Link>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 className="text-3xl font-bold text-purple-900 mb-8 text-center mt-10">Featured Products</h1>
+                <div className="flex">
+                    <div className="W-[49%] bg-amber-300">
+                        <h1 className="text-3xl font-bold text-purple-900 mb-8 text-center mt-10">Featured Products</h1>
+                    </div>
+                    <div className="W-[49%]"><CardSlider /></div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -13,7 +13,7 @@ export default function HomePage() {
         <div className="min-h-screen flex flex-col bg-gray-50">
             <Header />
             <main className="flex-1">
-                <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+                <div className="mx-auto w-full py-8">
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/contact" element={<Contact />} />
